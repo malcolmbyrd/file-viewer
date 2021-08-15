b416cd48a160069c7fbd7b7e2127e677819be630
mbyrd <mbyrd@trhc.com>
2021-08-15 17:43:11 -0400

    Hide everything except navigation buttons below table... really we could overwrite the columns prop of DataGrid, but this is the next best thing

--- client/src/components/DataTable.tsx
+++ client/src/components/DataTable.tsx
@@ -1,23 +1,15 @@
 import * as React from 'react';
 import {DataGrid} from '@material-ui/data-grid';
 import {useListEntriesQuery} from "../generated-api";
-import {makeStyles} from "@material-ui/core/styles";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
 import Button from "@material-ui/core/Button";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import SubdirectoryArrowRightIcon
   from "@material-ui/icons/SubdirectoryArrowRight";
-
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-});
+import {useEffect} from "react";
 
 const DataTable = () => {
-  const classes = useStyles();
-  const [sizeGt, setSizeGt] = React.useState(200);
   const [page, setPage] = React.useState(1);
   const [currentPath, setCurrentPath] = React.useState('/')
   const [history, updateHistory] = React.useState<{ id: string, path: string }[]>(
@@ -53,7 +45,7 @@ const DataTable = () => {
     },
   ];
 
-  const {data, loading, error} = useListEntriesQuery({
+  const {data} = useListEntriesQuery({
     variables: {
       path: currentPath,
       page,
@@ -81,7 +73,7 @@ const DataTable = () => {
     },
   });
 
-  React.useEffect(() => {
+  useEffect(() => {
     setCurrentPath(history[history.length - 1].path)
   }, [history])
 
@@ -103,28 +95,9 @@ const DataTable = () => {
     ]
   }, [history.length, data?.listEntries?.entries])
 
-
-  const rowCount = React.useMemo(() => {
-    const totalUpDirRows = currentPath === '/'
-      ? 0
-      : (data?.listEntries?.pagination.pageCount ?? 0) * 1
-    const totalRowsFromServer = data?.listEntries?.pagination.totalRows ?? 0
-    return totalRowsFromServer + totalUpDirRows
-  }, [
-    data?.listEntries?.pagination.pageCount,
-    data?.listEntries?.pagination.totalRows
-  ])
-
-  const handleChangePage = (event: unknown, newPage: number) => {
-    setPage(newPage + 1);
-  };
-
-  const handleDelete = () => {
-    setSizeGt(0)
-  }
-
   return (
     <div style={{height: 400, width: '100%'}}>
+      <h1>File Viewer</h1>
       <DataGrid
         rows={rows}
         columns={columns}
@@ -132,36 +105,36 @@ const DataTable = () => {
         checkboxSelection
         disableSelectionOnClick
       />
+      <h2>Navigation</h2>
       <table>
-      {rows.map(({path, __typename, name, size, id }) => {
+      {rows.map(({path, __typename, id }) => {
         const isUpDir = __typename === 'UP_DIR'
         return (
           <TableRow key={id}>
+            {(__typename !== 'File') && (
             <TableCell component="th" scope="row">
-              <Button
-                color="primary"
-                disabled={__typename === 'File'}
-                startIcon={isUpDir
-                  ? (<MoreHorizIcon />)
-                  : (__typename === 'File' ? null : <SubdirectoryArrowRightIcon />)
-                }
-                onClick={() => {
-                  updateHistory((h) => {
-                    if (isUpDir && h.length > 1) {
-                      setPage(1)
-                      return [...h.splice(0, h.length - 1)]
-                    } else {
-                      return ([...h, { id: path, path }])
-                    }
-                  })
-                }}
-              >
-                {!isUpDir ? path : ''}
-              </Button>
+                <Button
+                  disabled={false}
+                  color="primary"
+                  startIcon={isUpDir
+                    ? (<MoreHorizIcon />)
+                    : (__typename === 'File' ? null : <SubdirectoryArrowRightIcon />)
+                  }
+                  onClick={() => {
+                    updateHistory((h) => {
+                      if (isUpDir && h.length > 1) {
+                        setPage(1)
+                        return [...h.splice(0, h.length - 1)]
+                      } else {
+                        return ([...h, { id: path, path }])
+                      }
+                    })
+                  }}
+                >
+                  {!isUpDir ? path : ''}
+                </Button>
             </TableCell>
-            <TableCell align="right">{isUpDir ? '_' : name}</TableCell>
-            <TableCell align="right">{isUpDir ? '_' : __typename}</TableCell>
-            <TableCell align="right">{isUpDir ? '_' : size}</TableCell>
+            )}
           </TableRow>
         )})}
       </table>
